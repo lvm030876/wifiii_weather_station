aa43f12ba777744d99cb9043b84378433c3a6e61
lvm030876 <lvm030876@gmail.com>
2022-06-03 13:52:07 +0300

    correct "help"

--- src/HELPHTM.h
+++ src/HELPHTM.h
@@ -8,7 +8,6 @@ const char* helpIndex = R"=====(
 		<h1 style="color: #5e9ca0;"><span style="color: #000000;">Команди пристроя</span></h1>
 		<div>
 			<ul>
-				<li><span style="color: #ff0000;">resalarm <span style="color: #000000;">- перезапуск спрацьованих датчиків</span></span></li>
 				<li><span style="color: #ff0000;">sensor.xml, sensor.json</span></li>
 				<li><span style="color: #ff0000;">ip.htm <span style="color: #000000;">- сторінка налаштунку статичного адресу</span></span></li>
 				<li><span style="color: #ff0000;">scanwifi.json</span></li>
